30f6b97e2692d747551c6be1f779fe5363898a34
Koki <kgoudy64@gmail.com>
2022-07-11 10:02:49 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        var doubleValue = 10_000_000.53;
+
+        var numF = NumberFormat.getNumberInstance();
+        System.out.println("Number: " + numF.format(doubleValue));
+
+        var intF = NumberFormat.getIntegerInstance();
+        System.out.println("Number: " + intF.format(doubleValue));
+
+        intF.setGroupingUsed(false);
+        System.out.println("Number: " + intF.format(doubleValue));
+
+
+    }
+}
